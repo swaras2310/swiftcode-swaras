0c59dbbab8dbe923ef78d58a2fd89979ca101bff
Live session user <swaras.nr@gmail.com>
2017-09-15 06:19:21 +0000

    third commit

---
+++ app/data/Message.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Message {
+    public String text;
+    public FeedResponse feedResponse;
+    public Sender sender;
+    public enum Sender{
+        BOT,USER
+    }
+
+
+}
---
+++ app/services/FeedService.java
@@ -0,0 +1,39 @@
+package services;
+
+import data.FeedResponse;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import play.libs.ws.WS;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+
+
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+public class FeedService {
+    public FeedResponse getFeedResponse(String keyword) {
+        FeedResponse feedResponseObject = new FeedResponse();
+        try {
+            WSRequest feedQuery = WS.url("http://www.news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedQuery
+                    .setQueryParameter("q", keyword)
+                    .setQueryParameter("output", "rss")
+                    .get();
+            Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+
+            Node item = feedResponse.getFirstChild().getFirstChild().getChildNodes().item(9);
+            feedResponseObject.title = item.getChildNodes().item(0).getNodeValue();
+            feedResponseObject.description = item.getChildNodes().item(4).getNodeValue();
+            feedResponseObject.pubDate = item.getChildNodes().item(3).getNodeValue();
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return feedResponseObject;
+    }
+}
