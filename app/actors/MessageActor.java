491487cde6cd783fb5e3d4e6914ecb24837524ac
Live session user <swaras.nr@gmail.com>
2017-09-16 07:52:35 +0000

    fifth commit

---
+++ app/actors/MessageActor.java
@@ -0,0 +1,50 @@
+package actors;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.FeedResponse;
+import data.Message;
+import services.AgentService;
+import services.FeedService;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class MessageActor extends UntypedActor {
+
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    private FeedService feedService = new FeedService();
+    private AgentService agentService = new AgentService();
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Message messageObject = new Message();
+        if (message instanceof String) {
+            messageObject.text = (String) message;
+            messageObject.sender = Message.Sender.USER;
+            out.tell(mapper.writeValueAsString(messageObject),
+                    self());
+            String keyword = agentService
+                    .getResponse((String) message).keyword;
+            if (!Objects.equals(keyword, "NOT_FOUND")) {
+                FeedResponse feedResponse = feedService.getFeedResponse(keyword);
+                messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + keyword;
+                messageObject.feedResponse = feedResponse;
+                messageObject.sender = Message.Sender.BOT;
+                out.tell(mapper.writeValueAsString(messageObject), self());
+            }
+        }
+    }
+}
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,4 +2,32 @@ var app=angular.module('chatApp',['ngMaterial']);
 
 app.controller('chatController',function ($scope){
 
+	$scope.messages=[
+	{
+		sender:"USER",
+		text:"Hi bot",
+		time:"6:02 PM"
+	},
+	{
+		sender:"BOT",
+		text:"oh",
+		time:"6:02 PM"
+	},
+	{
+		sender:"BOT",
+		text:"Hi",
+		time:"6:02 PM"
+	},
+	{
+		sender:"USER",
+		text:"Hi",
+		time:"6:02 PM"
+	}
+	];
+
+    var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+    exampleSocket.onmessage  =   function  (event) {
+           var jsonData = JSON.parse(event.data);
+           console.log(jsonData);
+       };
 })
